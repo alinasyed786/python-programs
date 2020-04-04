7cb06f27f6679923381452dcc937798766d1b6e8
alinasyed786 <59252562+alinasyed786@users.noreply.github.com>
2020-04-04 12:05:46 +0530

    Create 2.py

---
+++ python assignment/2.py
@@ -0,0 +1,11 @@
+#2.Write a python class which has two methods get string and print string.get string accept a string from the user and print string print the string in UPPERCASE.
+class String:
+    def get_string(self):
+        string=input("Enter the string\n")
+        return string
+    def print_string(self,strr):
+        print(strr.upper())
+
+obj=String()
+strr=obj.get_string()
+obj.print_string(strr
