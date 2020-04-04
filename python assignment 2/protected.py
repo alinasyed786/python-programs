2cfd707c8bc532447d6f9fe5ebcdf250f89d22cc
alinasyed786 <59252562+alinasyed786@users.noreply.github.com>
2020-04-04 12:39:00 +0530

    Create protected.py

---
+++ python assignment 2/protected.py
@@ -0,0 +1,7 @@
+class Modifiers:
+
+    def __init__(self,name):
+        self._protected_member = name                                    # Protected Attribute
+       
+m = Modifiers("python")
+print(m._protected_member)
