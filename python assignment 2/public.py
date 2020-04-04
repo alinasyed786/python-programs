017ace7b6f2b31d6ca7ce85bd9bab767e2d5f44a
alinasyed786 <59252562+alinasyed786@users.noreply.github.com>
2020-04-04 12:36:07 +0530

    Create public.py

---
+++ python assignment 2/public.py
@@ -0,0 +1,10 @@
+# public acces modifier
+class Modifiers:
+
+    def __init__(self,name):
+        self.public_member = name
+
+mod = Modifiers("python")
+print(mod.public_member)                #Accessing the Public Attribute of Modifier Class
+mod.public_member = "Github"            #Changing it's value
+print(mod.public_member)
